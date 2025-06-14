1b2af142a324ba0bc61c1458c758b90bf29ecfe8
an-kit13 <ankitkrsingh48@gmail.com>
2025-06-14 16:50:13 +0530

    global url added

--- client/src/Components/NewCollections/NewCollections.jsx
+++ client/src/Components/NewCollections/NewCollections.jsx
@@ -5,7 +5,7 @@ const NewCollections = () => {
   const [new_collection, setNew_Collection] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:4000/newcollections")
+    fetch("https://thaam.onrender.com/newcollections")
       .then((response) => response.json())
       .then((data) => setNew_Collection(data));
   }, []);
--- client/src/Pages/LoginSignup.jsx
+++ client/src/Pages/LoginSignup.jsx
@@ -16,7 +16,7 @@ const LoginSignUp = () => {
   const login = async () => {
     console.log("Login Function Executed", formData);
     let responseData;
-    await fetch("http://localhost:4000/login", {
+    await fetch("https://thaam.onrender.com/login", {
       method: "POST",
       headers: {
         Accept: "Application/form-data",
@@ -38,7 +38,7 @@ const LoginSignUp = () => {
   const signup = async () => {
     console.log("Signup Function Executed", formData);
     let responseData;
-    await fetch("http://localhost:4000/signup", {
+    await fetch("https://thaam.onrender.com/signup", {
       method: "POST",
       headers: {
         Accept: "Application/form-data",
